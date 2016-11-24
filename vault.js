4c9d663ebcb2884ccf8cddee4f35ebd5efecb80c
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-11-23 17:41:15 -1000

    completed

--- vault.js
+++ vault.js
@@ -1,4 +1,34 @@
 'use strict';
-module.exports = function() {
+module.exports = function () {
+
+var secretStuff = {};
+
+  function _setValue(key, value){
+
+  secretStuff[key]= value;
+
+  }
+
+
+
+  function _getValue (key){
+
+    if(secretStuff[key]){
+      return secretStuff[key]; 
+        
+      } else {
+        return null;
+
+      }
+    }
+       
+
+  
+
+  return {
+    setValue: _setValue,
+    getValue: _getValue
+
+  };
   
 };
